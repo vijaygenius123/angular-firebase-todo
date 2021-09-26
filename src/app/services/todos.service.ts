aa099ba14a81ea68b1b423d2d1d208876276d113
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-09-26 11:17:21 +0100

    Add Todos Service Base

---
+++ src/app/services/todos.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from "@angular/fire/compat/firestore";
+import {Todo} from "../models/todo.model";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodosService {
+
+  todosCollection: AngularFirestoreCollection<Todo>
+  //todoDoc: AngularFirestoreDocument<Todo>
+  todos: Observable<Todo[]>
+
+
+  constructor(private fs: AngularFirestore) {
+    this.todosCollection = this.fs.collection('todos')
+    this.todos = this.todosCollection.snapshotChanges()
+      .pipe(
+        map(changes => {
+          return changes.map(d => {
+            const data = d.payload.doc.data() as Todo
+            data.id = d.payload.doc.id
+            return data
+          })
+        })
+      )
+  }
+}
